842654923e89d2c0fc041cf178badb003c901a9f
manescianera <nakas.zygimantas@gmail.com>
2021-05-03 10:31:22 +0300

    add vuex store

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,39 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import createPersistedState from 'vuex-persistedstate'
+import _ from 'lodash'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+	plugins: [createPersistedState({
+		storage: window.sessionStorage,
+	})],
+	state: {
+    	comments: [],
+    	maxVisible: 4
+  	},
+	getters: {
+		visibleComments: state => _.takeRight(state.comments, state.maxVisible),
+		hiddenCommentsCount: state => 
+			state.comments.length > state.maxVisible 
+				? state.comments.length - state.maxVisible 
+				: 0
+  	},
+	mutations: {
+    	ADD_COMMENT: (state, comment) => state.comments.push(comment),
+    	REMOVE_COMMENT: (state, comment) => 
+			state.comments.splice(
+				state.comments.indexOf(comment), 1
+			),
+		INCREMENT_MAX_VISIBLE: (state, count) => state.maxVisible += count,
+  	},
+	actions: {
+		addComment: (context, comment) => {
+	    	context.commit('ADD_COMMENT', comment)
+		},
+		removeComment: (context, comment) => {
+	    	context.commit('REMOVE_COMMENT', comment)
+	  	},
+	},
 })
