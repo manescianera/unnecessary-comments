2c1203c7ef5172e3e9d58400e2382a1ad0159c2e
manescianera <nakas.zygimantas@gmail.com>
2021-05-03 12:48:10 +0300

    rm console log :)

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,7 +1,6 @@
 // Scroll element into view by css selector.
 
 export function scrollIntoView(q) {
-    console.log('scroll')
     const el = document.querySelector(q)
     if (el) {
         setTimeout(() => {
